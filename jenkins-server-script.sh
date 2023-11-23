9668a252fe4b5589780c557b5cae3fe513c480c1
zeba akhtari <zeba.akhtari@abzooba.com>
2023-11-23 19:49:41 +0530

    init

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -2,10 +2,10 @@
 
 # install jenkins
 
-sudo yum update
+sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade -y
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
 sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
